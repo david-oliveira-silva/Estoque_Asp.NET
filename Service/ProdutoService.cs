5db89cce7bbb86b476a507682022e315442ed5e3
David Oliveira Silva <davidoliveira3537@gmail.com>
2025-08-16 11:35:59 -0300

    Criação da regra de negócio na camada SERVICE

---
+++ Service/ProdutoService.cs
@@ -0,0 +1,49 @@
+﻿using ProjetoWeb.Dao;
+using ProjetoWeb.Models;
+
+namespace ProjetoWeb.Service
+{
+    public class ProdutoService
+    {
+        private IProdutoDAO produtoDAO;
+
+        public ProdutoService(IProdutoDAO produtoDAO)
+        {
+            this.produtoDAO = produtoDAO;
+        }
+
+        public void CadastrarProduto(string nomeProduto, decimal valorProduto)
+        {
+
+            if (string.IsNullOrEmpty(nomeProduto))
+            {
+                throw new Exception("não do produto não pode ser vazio");
+            }
+            if (valorProduto < 0)
+            {
+                throw new Exception("produto não pode ser negativo");
+            }
+
+            var novoProduto = new ProdutoModel(nomeProduto, valorProduto);
+            produtoDAO.cadastrarProduto(novoProduto);
+        }
+
+        public void RemoverProduto(ProdutoModel produtoModel)
+        {
+            if (produtoModel == null)
+            {
+
+                throw new Exception("Produto não encontrado");
+            }
+
+            produtoDAO.removerProduto(produtoModel);
+        }
+
+        public List<ProdutoModel> listarProdutos()
+        {
+
+             var produto = produtoDAO.listarProduto();
+            return produto.OrderBy(p => p.produtoID).ToList();
+        }
+    }
+}

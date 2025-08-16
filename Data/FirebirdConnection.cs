9f73c630969a920a3445890c6f378d4af09bfede
David Oliveira Silva <davidoliveira3537@gmail.com>
2025-08-16 10:56:43 -0300

    Primeiro commit

---
+++ Data/FirebirdConnection.cs
@@ -0,0 +1,45 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+
+namespace ProjetoWeb.Data
+{
+    public class FirebirdConnection
+    {
+
+        private static readonly string conexaoString;
+
+        public static FbConnection GetFbConnection()
+        {
+
+            return new FbConnection(conexaoString);
+        }
+
+
+        public static void OpenConnection(FbConnection fbConnection)
+        {
+
+            try
+            {
+                fbConnection.Open();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void CloseConnection(FbConnection fbConnection)
+        {
+
+            try
+            {
+                fbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}

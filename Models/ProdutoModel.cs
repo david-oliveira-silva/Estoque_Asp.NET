fb2694ba2f7fc51350ed700f4ca030d474456772
David Oliveira Silva <davidoliveira3537@gmail.com>
2025-08-16 11:07:32 -0300

    Criação do Model Produto com propriedades e construtor

---
+++ Models/ProdutoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoWeb.Models
+{
+    public class ProdutoModel
+    {
+        public int produtoID {  get; set; }
+        public string nomeProduto { get; set; }
+        public decimal valorProduto {  get; set; }
+    }
+}
